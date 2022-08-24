600966914c33e56f3e711cf116114624104220da
AlexSergey <gooddev.sergey@gmail.com>
2022-08-24 12:59:19 +0300

    fix: lint-staged

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,9 +1,9 @@
 module.exports = {
   'package.json': ['npm run format:package'],
-  'src/**/*.{ts}': [() => 'npm run lint:ts'],
-  'test/**/*.{ts}': [() => 'npm run lint:ts'],
-  'prisma/**/*.{ts}': [() => 'npm run lint:ts'],
-  'types/**/*.{ts}': [() => 'npm run lint:ts'],
+  'src/**/*.ts': [() => 'npm run lint:ts'],
+  'test/**/*.ts': [() => 'npm run lint:ts'],
+  'prisma/**/*.ts': [() => 'npm run lint:ts'],
+  'types/**/*.ts': [() => 'npm run lint:ts'],
   'src/**/*.{ts,json}': ['prettier --write', 'eslint --fix'],
   'test/**/*.{ts,json}': ['prettier --write', 'eslint --fix'],
   'prisma/**/*.{ts,json}': ['prettier --write', 'eslint --fix'],
