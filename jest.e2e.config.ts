02871ff95348eaae82d19d50271e94994de256be
AlexSergey <gooddev.sergey@gmail.com>
2022-11-19 12:30:19 +0200

    fix: fix e2e config

--- jest.e2e.config.ts
+++ jest.e2e.config.ts
@@ -1,15 +1,16 @@
 import type { Config } from '@jest/types';
 // eslint-disable-next-line import/no-extraneous-dependencies
 import { pathsToModuleNameMapper } from 'ts-jest';
+import { loadConfig } from 'tsconfig-paths';
 
-import { compilerOptions } from './tsconfig.json';
+const tsconfig = loadConfig() as { paths: Record<string, string[]> };
 
 const config: Config.InitialOptions = {
   collectCoverageFrom: ['**/*.(t|j)s'],
   coverageDirectory: '<rootDir>/coverage/e2e',
   moduleDirectories: ['node_modules', 'src'],
   moduleFileExtensions: ['js', 'json', 'ts'],
-  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths) as { [key: string]: string | string[] },
+  moduleNameMapper: pathsToModuleNameMapper(tsconfig.paths) as { [key: string]: string | string[] },
   preset: 'ts-jest',
   roots: ['<rootDir>/src', '<rootDir>/test'],
   setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
